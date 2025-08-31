718144227de29ad55008192710e4ac47c18f08f3
krishna <kkriss4683@gmail.com>
2025-08-31 16:53:32 +0530

    add company and job controllers, routes, and integrate with middleware for CRUD operations

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-// import userRoute from "./routes/user.route.js";
-// import companyRoute from "./routes/company.route.js";
-// import jobRoute from "./routes/job.route.js";
-// import applicationRoute from "./routes/application.route.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
 
@@ -26,11 +26,11 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
 
-// // api's
-// app.use("/api/v1/user", userRoute);
-// app.use("/api/v1/company", companyRoute);
-// app.use("/api/v1/job", jobRoute);
-// app.use("/api/v1/application", applicationRoute);
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 
 
