6fc8a64a9871eb3dd83acce1d8880fa34797997f
krishna <kkriss4683@gmail.com>
2025-09-16 04:38:57 +0530

    ccc

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  // origin:'http://localhost:5173',
-  origin: [
-    "https://job-portal-frontend-54ol.onrender.com",
-    "http://localhost:5173",
-  ],
+  origin:'http://localhost:5173',
+  // origin: [
+  //   "https://job-portal-frontend-54ol.onrender.com",
+  //   "http://localhost:5173",
+  // ],
   credentials: true,
 };
 
